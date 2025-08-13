541b42b8f3f13712466ed712ff717fe0b682f95c
JuanMaDev <juanmarcoso@gmail.com>
2025-08-12 21:37:52 -0300

    JavaScript - Empezando con E-commerce

---
+++ JavaScript/E-Commerce/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quantity: 1,
+        image: "/E-Commerce/client/media/banana.jpg",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quantity: 1,
+        image: "/E-Commerce/client/media/leche.jpg",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quantity: 1,
+        image: "/E-Commerce/client/media/pollo.jpg",
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 510,
+        quantity: 1,
+        image: "/E-Commerce/client/media/mayonesa.jpg",
+    }
+]
