6cea34da1617ba66a5c7d4502521ba34b0be6fa8
JuanMaDev <juanmarcoso@gmail.com>
2025-09-14 12:01:54 -0300

    Node - Agregando Clase02

---
+++ Node/01-Hola-Mundo/app.js
@@ -0,0 +1,15 @@
+let nombre = "Juan";
+console.log("Hola, " + nombre + "! Bienvenido a tu primera aplicación de Node.js.");
+
+nombre = "María";
+console.log("Hola, " + nombre + "! Bienvenida a tu primera aplicación de Node.js.");
+
+// const nombre = "Juan";
+// console.log(nombre);
+
+// const nombre2 = "María";
+// console.log(nombre2);
+
+// Puedes ejecutar este archivo con el comando: node app.js
+// Asegúrate de estar en el directorio correcto donde se encuentra este archivo.
+// Este es un ejemplo simple de una aplicación en Node.js que imprime un saludo en la consola.
