cc55b0fecc2f054bbf87e0d5c43a997398cc88ba
JuanMaDev <juanmarcoso@gmail.com>
2025-09-15 20:13:04 -0300

    Python - Agregando Clase06

---
+++ Python/Clase06/dar_formato_str.py
@@ -0,0 +1,10 @@
+# Dar formato a una cadena de texto
+
+nombre = "Juan"
+edad = 28
+
+mensaje_con_formato = 'Mi nombre es %s y tengo %d años.' % (nombre, edad) 
+# Siempre respetar el orden de las variables
+print(mensaje_con_formato)
+# mensaje_con_formato = "Hola, mi nombre es {} y tengo {} años.".format(nombre, edad)
+# print(mensaje_con_formato)
