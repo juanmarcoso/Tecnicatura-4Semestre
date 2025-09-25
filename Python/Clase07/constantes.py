a9767f8ae592392266f34cc708e9e220a87462c1
JuanMaDev <juanmarcoso@gmail.com>
2025-09-25 10:19:09 -0300

    Python - Agregando comienzo juego Clase07

---
+++ Python/Clase07/constantes.py
@@ -0,0 +1,11 @@
+import os
+
+# Dimension de la pantalla, el alto y el ancho de la pantalla
+SCREEN_WITH = 820
+SCREEN_HEIGHT = 600
+
+# Especificamos los colores de la pantalla
+COLOR_LASER = (0,0,255) # Azul
+
+# Ruta a la carpeta
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets')
---
+++ Python/Clase07/main.py
@@ -0,0 +1,7 @@
+import pygame
+import sys
+import random
+import os
+
+from personaje import Personaje, Enemigo, Explosion
+from constantes import 
