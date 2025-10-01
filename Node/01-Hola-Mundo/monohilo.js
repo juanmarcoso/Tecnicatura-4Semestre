66f3d16be9197e9816a9a4c0f38bef2e3423a0af
JuanMaDev <juanmarcoso@gmail.com>
2025-10-01 20:45:50 -0300

    Node - Clase05

---
+++ Node/01-Hola-Mundo/monohilo.js
@@ -0,0 +1,13 @@
+console.log('Hola a todos!')
+
+var i = 0;
+setInterval(function(){
+    console.log('Sigo activo: ' + i);
+    i++;
+
+    // if( i === 5){
+    //     var a = 3+Z;
+    // }
+}, 1000);
+
+console.log('Segunda instruccion');
