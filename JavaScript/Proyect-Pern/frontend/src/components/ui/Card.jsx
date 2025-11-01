53f1e0ab65f025a5e8fd46217e5bb0c0119c4011
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-11-01 19:32:54 -0300

    JavaScript - odificado proyecto PERN

--- JavaScript/Proyect-Pern/frontend/src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Card({children, className}) {
-  return (
-    <div className={`bg-zinc-900 p-14 rounded-md ${className}`}>{children}</div>
-  )
-}
-
-export default Card
