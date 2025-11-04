eee0d6fab2051b5737b7f31f5710bc63a223abbe
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-11-04 09:26:41 -0300

    Python - Agregado juego

--- Python/Clase07/main.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import sys
-import random
-import os
-
-from personaje import Personaje, Enemigo, Explosion
-from constantes import 
---
+++ Python/Clase070809/constantes.py
@@ -0,0 +1,12 @@
+import os
+
+
+# Dimensiones de la pantal
+SCREEN_WIDTH = 1700
+SCREEN_HEIGHT = 1000
+
+# Colore
+COLOR_LASER = (0, 0, 255)  # Azul
+
+# Ruta assets
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets_1')
